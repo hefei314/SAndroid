902f4228937a39a0174ddce2673775d45a722ffe
hefei <1349177631@qq.com>
2022-07-06 17:04:17 +0800

    update resources

---
+++ app/src/main/java/com/hefei/sandroid/modules/widget/WidgetTestActivity.java
@@ -0,0 +1,24 @@
+package com.hefei.sandroid.modules.widget;
+
+import com.hefei.sandroid.databinding.ActivityWidgetTestBinding;
+import com.hefei.sandroid.support.base.BaseBindingActivity;
+
+/**
+ * <pre>
+ *     author: hefei
+ *     time  : 2022/7/6
+ *     desc  :
+ * </pre>
+ */
+public class WidgetTestActivity extends BaseBindingActivity<ActivityWidgetTestBinding> {
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void updateViews() {
+
+    }
+}
